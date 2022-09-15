94a3c376097a5bc50564d2148fc340918c053600
Blastose <blastoisejay@gmail.com>
2022-09-14 19:00:26 -0700

    Display submission contents and comments

---
+++ src/routes/r/[subreddit]/comments/[submissionId]/+page.server.ts
@@ -0,0 +1,29 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { CLIENT_ID } from '$env/static/private';
+import { CLIENT_SECRET } from '$env/static/private';
+import snoowrap from 'snoowrap';
+import type Snoowrap from 'snoowrap';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const r = await snoowrap.fromApplicationOnlyAuth({
+		userAgent: 'Cygnet',
+		clientId: CLIENT_ID,
+		clientSecret: CLIENT_SECRET,
+		grantType: 'client_credentials'
+	});
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const submission = await r.getSubmission(params.submissionId).expandReplies({
+		limit: 1
+	});
+
+	if (submission) {
+		return {
+			submission: JSON.parse(JSON.stringify(submission)) as Snoowrap.Submission[]
+		};
+	}
+
+	throw error(404, 'Not found');
+};
---
+++ src/routes/r/[subreddit]/comments/[submissionId]/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import type Snoowrap from 'snoowrap';
+
+	export let data: any;
+	let { submission }: { submission: Snoowrap.Submission } = data;
+	// console.log(submission);
+</script>
+
+<main class="container mx-auto px-4">
+	<div class="flex flex-col gap-2">
+		<div>
+			<span class="font-bold text-2xl">{@html submission.title}</span>
+			{@html submission.selftext_html ?? ''}
+		</div>
+
+		<div>
+			<span class="font-bold text-lg">Comments: </span>
+			{#each submission.comments as comment}
+				<div class="flex gap-2">
+					<div>
+						<span class="font-semibold">{comment.author}</span>
+						<span>
+							{@html comment.body_html}
+						</span>
+						<div class="pl-6">
+							{#each comment.replies as reply}
+								<span class="font-semibold">{reply.author}</span>
+								<span>
+									{@html reply.body_html}
+								</span>
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</main>
