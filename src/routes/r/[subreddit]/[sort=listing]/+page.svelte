257d448f4206c3811f78abb74bc5752ad80146a0
Blastose <blastoisejay@gmail.com>
2022-09-15 18:55:34 -0700

    Use `Submission` component for listings

--- src/routes/r/[subreddit]/[sort=listing]/+page.svelte
+++ src/routes/r/[subreddit]/[sort=listing]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Submission from '$lib/components/Submission.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -34,13 +35,7 @@
 			>
 		</div>
 		{#each data.posts as post}
-			<div class="flex gap-2">
-				<span>{post.ups}</span>
-				<a class="text-blue-700" href={`/r/${data.posts[0]?.subreddit}/comments/${post.id}`}>
-					<span>{post.title}</span>
-				</a>
-				<span>{post.num_comments} comments</span>
-			</div>
+			<Submission submission={post} />
 		{/each}
 	</div>
 </main>
