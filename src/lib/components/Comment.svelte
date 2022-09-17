4f3bdb5a6d5da9aba9458ba1305fda236fbf871e
Blastose <blastoisejay@gmail.com>
2022-09-16 18:42:57 -0700

    Highlight name of submitter in comments

--- src/lib/components/Comment.svelte
+++ src/lib/components/Comment.svelte
@@ -3,10 +3,21 @@
 	export let comment: Snoowrap.Comment;
 </script>
 
-<div class="flex flex-col gap-2">
+<div
+	class="flex flex-col gap-2 
+  {comment.replies.length > 0 ? 'border-l' : ''} border-gray-200 rounded-tl-md"
+>
 	<div class="bg-gray-200 p-2 rounded-md">
-		<div class="flex gap-2">
-			<span class="text-blue-800 font-semibold">{comment.author}</span>
+		<div class="flex gap-2 text-sm">
+			<p>
+				<span
+					class="{comment.is_submitter
+						? 'text-white bg-blue-500 rounded-sm px-1 py-[0.075rem]'
+						: 'text-blue-800'} font-semibold"
+				>
+					{comment.author}
+				</span>
+			</p>
 			<span class="font-semibold">{comment.score} points</span>
 			<span>{new Date(comment.created_utc * 1000).toLocaleString()}</span>
 		</div>
