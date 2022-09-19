734df5634abf8146f6ecc16e27babd90f80414b6
Blastose <blastoisejay@gmail.com>
2022-09-19 11:43:39 -0700

    Add header (#5)
    * Add header
    
    * Add placeholders for header buttons
    
    * Remove unneeded tags

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<header class="sticky top-0 shadow-sm bg-white">
+	<div class="container mx-auto p-4 flex justify-between items-center">
+		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
+		<div class="flex gap-2">
+			<button>switch mode</button>
+			<button>Login</button>
+		</div>
+	</div>
+</header>
