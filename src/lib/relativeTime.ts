fd9545acfa8a94691e342e196acd3d76bb60b738
Blastose <blastoisejay@gmail.com>
2022-09-19 13:42:39 -0700

    Create `relativeTime` function

---
+++ src/lib/relativeTime.ts
@@ -0,0 +1,32 @@
+const unitsInSeconds = {
+	year: 365 * 24 * 60 * 60 * 1,
+	month: (365 / 12) * 24 * 60 * 60 * 1,
+	day: 24 * 60 * 60 * 1,
+	hour: 60 * 60 * 1,
+	minute: 60 * 1,
+	second: 1
+};
+
+const pluralize = (word: string, amount: number) => {
+	return amount > 1 ? `${word}s` : word;
+};
+
+const formatRelativeTimeOutput = (difference: number, unit: string) => {
+	return `${difference} ${pluralize(unit, difference)} ago`;
+};
+
+const relativeTime = (unixTimestampSeconds: number): string => {
+	const unixTimeNowSeconds = Math.floor(Date.now() / 1000);
+	const differenceInSeconds = unixTimeNowSeconds - unixTimestampSeconds;
+
+	for (const key in unitsInSeconds) {
+		const unitValue = unitsInSeconds[key as keyof typeof unitsInSeconds];
+		if (differenceInSeconds > unitValue) {
+			return formatRelativeTimeOutput(Math.floor(differenceInSeconds / unitValue), key);
+		}
+	}
+
+	return 'unknown time ago';
+};
+
+export default relativeTime;
