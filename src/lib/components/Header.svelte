4e29e727b5e4fdd2a1ba50a30877aa5927eacda1
Blastose <blastoisejay@gmail.com>
2022-09-19 16:52:58 -0700

    Add time filters to listings (#9)
    * Change time range of posts based on url search param
    
    * Change z-index
    
    * Add css dropdown menu to select time range
    
    * Remove dropdown expanding on hover
    
    * Adjust button padding

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<header class="sticky top-0 shadow-sm bg-white">
+<header class="sticky top-0 z-50 shadow-sm bg-white">
 	<div class="container mx-auto p-4 flex justify-between items-center">
 		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
 		<div class="flex gap-2">
--- src/lib/components/Listing.svelte
+++ src/lib/components/Listing.svelte
@@ -1,23 +1,68 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	const listings = ['hot', 'new', 'rising', 'controversial', 'top'];
+	const times = ['all', 'hour', 'day', 'week', 'month', 'year'];
 
 	let currentListing: string | undefined = undefined;
 	$: currentListing = $page.url.pathname.split('/')[3];
 
+	$: currentTimeFilter = $page.url.searchParams.get('t');
+
 	export let subreddit: string;
+
+	const formatTimeFilter = (time: string | null) => {
+		if (time === null) {
+			time = 'day';
+		}
+		return `${time !== 'all' ? 'past' : ''} ${time} ${time === 'all' ? 'time' : ''}`;
+	};
 </script>
 
-<nav class="flex">
-	{#each listings as listing}
-		<a
-			class="capitalize py-1 px-2 duration-150
+<div class="flex gap-2 items-center flex-wrap">
+	<nav class="flex">
+		{#each listings as listing}
+			<a
+				class="capitalize py-1 px-2 duration-150
       {currentListing === listing || (currentListing === undefined && listing === 'hot')
-				? 'bg-gray-500 text-white'
-				: 'bg-gray-300 hover:bg-gray-400'}"
-			href={listing === 'hot' ? `/r/${subreddit}` : `/r/${subreddit}/${listing}`}
-		>
-			{listing}
-		</a>
-	{/each}
-</nav>
+					? 'bg-gray-500 text-white'
+					: 'bg-gray-300 hover:bg-gray-400'}"
+				href={listing === 'hot' ? `/r/${subreddit}` : `/r/${subreddit}/${listing}`}
+			>
+				{listing}
+			</a>
+		{/each}
+	</nav>
+
+	{#if currentListing === 'controversial' || currentListing === 'top'}
+		<div class="flex items-center">
+			<span>links from: </span>
+			<div class="dropdown">
+				<button class="px-2">{formatTimeFilter(currentTimeFilter)}▾</button>
+				<nav class="dropdown-items flex flex-col bg-gray-300">
+					{#each times as time}
+						<a
+							class="p-2 hover:bg-gray-400 duration-150"
+							href="/r/{subreddit}/{currentListing}?t={time}">{formatTimeFilter(time)}</a
+						>
+					{/each}
+				</nav>
+			</div>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.dropdown {
+		position: relative;
+	}
+
+	.dropdown-items {
+		display: none;
+		width: max-content;
+		position: absolute;
+	}
+
+	.dropdown:focus-within .dropdown-items {
+		display: flex;
+	}
+</style>
