1d1e4a849dd176885d02b564386d391778bc4b71
Blastose <blastoisejay@gmail.com>
2022-09-19 12:17:13 -0700

    Create `Subreddit` component

---
+++ src/lib/components/Subreddit.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Listing from '$lib/components/Listing.svelte';
+	import Submission from '$lib/components/Submission.svelte';
+	import type Snoowrap from 'snoowrap';
+
+	export let subreddit: string;
+	export let submissions: Snoowrap.Submission[];
+</script>
+
+<div class="flex flex-col gap-2">
+	<span class="font-bold text-2xl">{subreddit}</span>
+	<Listing {subreddit} />
+	{#each submissions as submission}
+		<Submission {submission} />
+	{/each}
+</div>
