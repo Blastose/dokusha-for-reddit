a035019b8dc1b49da00a45efe3e8264e1e971dae
Blastose <blastoisejay@gmail.com>
2022-09-19 12:30:15 -0700

    Create `Listing` component (#6)
    * Create `Listing` component
    
    * Create `Subreddit` component
    
    * Use `Subreddit` component instead of copy-pasting code
    
    * Add transition-duration to listing options

---
+++ src/lib/components/Listing.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const listings = ['hot', 'new', 'rising', 'controversial', 'top'];
+
+	let currentListing: string | undefined = undefined;
+	$: currentListing = $page.url.pathname.split('/')[3];
+
+	export let subreddit: string;
+</script>
+
+<nav class="flex">
+	{#each listings as listing}
+		<a
+			class="capitalize py-1 px-2 duration-150
+      {currentListing === listing || (currentListing === undefined && listing === 'hot')
+				? 'bg-gray-500 text-white'
+				: 'bg-gray-300 hover:bg-gray-400'}"
+			href={listing === 'hot' ? `/r/${subreddit}` : `/r/${subreddit}/${listing}`}
+		>
+			{listing}
+		</a>
+	{/each}
+</nav>
--- src/routes/r/[subreddit]/[sort=listing]/+page.svelte
+++ src/routes/r/[subreddit]/[sort=listing]/+page.svelte
@@ -1,41 +1,15 @@
 <script lang="ts">
-	import Submission from '$lib/components/Submission.svelte';
+	import Subreddit from '$lib/components/Subreddit.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	let subreddit = data.posts[0]?.subreddit as unknown as string;
 </script>
 
 <svelte:head>
-	<title>{data.posts[0]?.subreddit}</title>
+	<title>{subreddit}</title>
 </svelte:head>
 
 <main class="container mx-auto px-4">
-	<div class="flex flex-col gap-2">
-		<span class="font-bold text-2xl">{data.posts[0]?.subreddit}</span>
-		<div class="flex gap-2">
-			<a
-				class="py-1 px-2 rounded-sm bg-gray-300 hover:bg-gray-400"
-				href="/r/{data.posts[0]?.subreddit}">Hot</a
-			>
-			<a
-				class="py-1 px-2 rounded-sm bg-gray-300 hover:bg-gray-400"
-				href="/r/{data.posts[0]?.subreddit}/new">New</a
-			>
-			<a
-				class="py-1 px-2 rounded-sm bg-gray-300 hover:bg-gray-400"
-				href="/r/{data.posts[0]?.subreddit}/rising">Rising</a
-			>
-			<a
-				class="py-1 px-2 rounded-sm bg-gray-300 hover:bg-gray-400"
-				href="/r/{data.posts[0]?.subreddit}/controversial">Controversial</a
-			>
-			<a
-				class="py-1 px-2 rounded-sm bg-gray-300 hover:bg-gray-400"
-				href="/r/{data.posts[0]?.subreddit}/top">Top</a
-			>
-		</div>
-		{#each data.posts as post}
-			<Submission submission={post} />
-		{/each}
-	</div>
+	<Subreddit {subreddit} submissions={data.posts} />
 </main>
