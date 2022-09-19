4326ac780b31650f00c42c922368ae615fb20154
Blastose <blastoisejay@gmail.com>
2022-09-19 11:37:24 -0700

    Add placeholders for header buttons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,8 +2,12 @@
 </script>
 
 <header class="sticky top-0 shadow-sm bg-white">
-	<div class="container mx-auto p-4">
+	<div class="container mx-auto p-4 flex justify-between items-center">
 		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
+		<div class="flex gap-2">
+			<button>switch mode</button>
+			<button>Login</button>
+		</div>
 	</div>
 </header>
 
