891202b2992a4dd074ab879760749013511e4b21
Blastose <blastoisejay@gmail.com>
2022-10-06 14:22:31 -0700

    Hide header on scroll

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,23 @@
-<header class="sticky top-0 z-50 shadow-sm bg-white">
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let hideHeader = false;
+
+	onMount(() => {
+		let prevScrollPos = window.scrollY;
+		window.onscroll = () => {
+			let currentScrollPos = window.scrollY;
+			if (prevScrollPos > currentScrollPos) {
+				hideHeader = false;
+			} else {
+				hideHeader = true;
+			}
+			prevScrollPos = currentScrollPos;
+		};
+	});
+</script>
+
+<header class="sticky top-0 z-50 shadow-sm bg-white duration-150 {hideHeader ? `-top-[60px]` : ''}">
 	<div class="container mx-auto p-4 flex justify-between items-center">
 		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
 		<div class="flex gap-2">
