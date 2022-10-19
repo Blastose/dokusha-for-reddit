e1e0619f8dd21c0de56250c5227dd6494e4769c6
Blastose <blastoisejay@gmail.com>
2022-10-18 17:53:11 -0700

    Add theme toggle

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { theme } from '$lib/stores/theme';
+	import { get } from 'svelte/store';
 
 	let hideHeader = false;
 
@@ -21,13 +23,27 @@
 			window.removeEventListener('scoll', changeHeaderVisibilityStatus);
 		};
 	});
+
+	const toggleTheme = () => {
+		const themeType = get(theme);
+		let newTheme: string;
+
+		if (themeType === 'dark') {
+			newTheme = 'light';
+		} else {
+			newTheme = 'dark';
+		}
+
+		theme.set(newTheme);
+		localStorage.setItem('theme', newTheme);
+	};
 </script>
 
 <header class="sticky top-0 z-50 shadow-sm bg-white duration-150 {hideHeader ? `-top-[60px]` : ''}">
 	<div class="container mx-auto p-4 flex justify-between items-center">
 		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
 		<div class="flex gap-2">
-			<button>switch mode</button>
+			<button on:click={toggleTheme}>switch mode</button>
 			<button>Login</button>
 		</div>
 	</div>
