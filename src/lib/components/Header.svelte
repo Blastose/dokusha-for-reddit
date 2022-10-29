e6e301a7167d355501f8655cf3f31a34855859e1
Blastose <blastoisejay@gmail.com>
2022-10-29 11:37:08 -0700

    Remove sticky header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,27 +3,6 @@
 	import { theme } from '$lib/stores/theme';
 	import { get } from 'svelte/store';
 
-	let hideHeader = false;
-
-	onMount(() => {
-		let prevScrollPos = window.scrollY;
-		const changeHeaderVisibilityStatus = () => {
-			let currentScrollPos = window.scrollY;
-			if (prevScrollPos > currentScrollPos) {
-				hideHeader = false;
-			} else {
-				hideHeader = true;
-			}
-			prevScrollPos = currentScrollPos;
-		};
-
-		window.addEventListener('scroll', changeHeaderVisibilityStatus);
-
-		return () => {
-			window.removeEventListener('scoll', changeHeaderVisibilityStatus);
-		};
-	});
-
 	const toggleTheme = () => {
 		const themeType = get(theme);
 		let newTheme: string;
@@ -39,10 +18,7 @@
 	};
 </script>
 
-<header
-	class="sticky top-0 z-50 shadow-sm bg-white dark:bg-[#212224] transition-[top] duration-150 
-	{hideHeader ? `-top-[60px]` : ''}"
->
+<header class="shadow-sm bg-white dark:bg-[#212224] transition-[top] duration-150">
 	<div class="container mx-auto p-4 flex justify-between items-center">
 		<a href="/" class="text-xl font-bold"> Dokusha for Reddit </a>
 		<div class="flex gap-2">
