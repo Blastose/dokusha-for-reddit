f216aa1b34eaadd14464ebb874824fead0b92a16
Blastose <blastoisejay@gmail.com>
2022-10-30 18:50:50 -0700

    Remove unused import

--- src/routes/r/[subreddit]/comments/[submissionId]/+page.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/+page.svelte
@@ -2,7 +2,6 @@
 	import Comment from '$lib/components/Comment.svelte';
 	import type Snoowrap from 'snoowrap';
 	import relativeTime from '$lib/relativeTime';
-	import Submission from '$lib/components/Submission.svelte';
 
 	export let data: { submission: Snoowrap.Submission };
 	console.log(data.submission);
