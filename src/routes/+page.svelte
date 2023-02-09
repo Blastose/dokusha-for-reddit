6cafe86c4dda09f1e1290c66f210cfcb08e9f2a0
Blastose <blastoisejay@gmail.com>
2023-02-08 17:26:12 -0800

    Add title to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 	export let data: PageData;
 </script>
 
+<svelte:head><title>Dokusha for Reddit</title></svelte:head>
+
 <main class="w-full sm:max-w-5xl mx-auto px-4">
 	<div class="flex flex-col gap-2">
 		<span class="font-bold text-2xl">The Front Page of Reddit</span>
