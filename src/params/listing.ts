2d25fe13d04a3cc7d0353ade3856667c6b608cc2
Blastose <blastoisejay@gmail.com>
2023-02-21 20:20:49 -0800

    Initial commit

--- src/params/listing.ts
+++
@@ -1,8 +0,0 @@
-import type { ParamMatcher } from '@sveltejs/kit';
-
-export const match: ParamMatcher = (param) => {
-	if (param === 'new' || param === 'rising' || param === 'controversial' || param === 'top') {
-		return true;
-	}
-	return false;
-};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,5 @@
-<script lang="ts">
-	import Submission from '$lib/components/Submission.svelte';
-	import type { PageData } from './$types';
-	export let data: PageData;
-</script>
-
-<svelte:head><title>Dokusha for Reddit</title></svelte:head>
-
-<main class="w-full sm:max-w-5xl mx-auto px-4">
-	<div class="flex flex-col gap-2">
-		<span class="font-bold text-2xl">The Front Page of Reddit</span>
-		{#each data.posts as submission}
-			<Submission {submission} showSubredditName={true} />
-		{/each}
-	</div>
-</main>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="text-[#3d46c9] hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+	read the documentation
+</p>
