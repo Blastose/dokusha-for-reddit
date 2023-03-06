0322c4b6c713e04124b80d732b8342a69c0f3e60
Blastose <blastoisejay@gmail.com>
2023-03-06 12:42:54 -0800

    Add theme toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
 	import '../app.css';
+	import Layout from '$lib/components/layout/Layout.svelte';
+	import { onMount } from 'svelte';
+	import { themeStore } from '$lib/stores/themeStore';
+
+	onMount(() => {
+		if (!('theme' in localStorage)) {
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				themeStore.set('dark');
+			} else {
+				themeStore.set('light');
+			}
+		} else {
+			if (localStorage.getItem('theme') === 'dark') {
+				themeStore.set('dark');
+			} else if (localStorage.getItem('theme') === 'light') {
+				themeStore.set('light');
+			}
+		}
+	});
 </script>
 
-<slot />
+<svelte:head>
+	<script>
+		if (!('theme' in localStorage)) {
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				document.documentElement.classList.add('dark');
+			}
+		} else {
+			if (localStorage.getItem('theme') === 'dark') {
+				document.documentElement.classList.add('dark');
+			}
+		}
+	</script>
+</svelte:head>
+
+<Layout>
+	<slot />
+</Layout>
