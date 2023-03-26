f8c196986b8c01bbbfabdaa20dc79f56fb416850
Blastose <blastoisejay@gmail.com>
2023-03-26 15:03:02 -0700

    Fix submissionStore not being set on back/forward navigation

--- src/routes/r/[subreddit]/comments/[submissionId]/[[submissionTitle]]/+page.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/[[submissionTitle]]/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Submission from '$lib/components/submission/Submission.svelte';
+	import { onMount } from 'svelte';
+	import { submissionStore } from '$lib/stores/submissionStore';
 
 	export let data: PageData;
+
+	onMount(async () => {
+		submissionStore.set(await data.streamed.submission);
+	});
 </script>
 
 <main class="container mx-auto px-4 pt-4 pb-24 ">
