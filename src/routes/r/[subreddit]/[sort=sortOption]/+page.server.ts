4dc5be1456952a716a1909fde4c68b17e9033a81
Blastose <blastoisejay@gmail.com>
2023-04-11 16:06:54 -0700

    Add sort options to routes

---
+++ src/routes/r/[subreddit]/[sort=sortOption]/+page.server.ts
@@ -0,0 +1,24 @@
+import { jsrwrap } from '$lib/server/reddit';
+import { error } from '@sveltejs/kit';
+
+type Time = 'hour' | 'day' | 'week' | 'month' | 'year' | 'all';
+
+export const load = async ({ params, setHeaders, url }) => {
+	const subreddit = params.subreddit;
+	const sort = params.sort as 'top' | 'new' | 'controversial' | 'rising';
+
+	let t = url.searchParams.get('t') as Time | null;
+	t = t ?? 'hour';
+
+	const jsrWrapsubreddit = jsrwrap.getSubreddit(subreddit);
+
+	const about = await jsrWrapsubreddit.getAbout();
+
+	const posts = await jsrWrapsubreddit.getSubmissions({ sort, params: { t } });
+
+	if (!posts) throw error(500);
+
+	setHeaders({ 'cache-control': 'public, max-age=60' });
+
+	return { posts, about };
+};
---
+++ src/routes/r/[subreddit]/[sort=sortOption]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Subreddit from '$lib/components/subreddit/Subreddit.svelte';
+
+	export let data;
+</script>
+
+<svelte:head><title>{data.about.title}</title></svelte:head>
+
+<main>
+	<Subreddit about={data.about} posts={data.posts} />
+</main>
