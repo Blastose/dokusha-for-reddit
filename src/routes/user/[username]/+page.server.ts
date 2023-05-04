b678c1ee231a9b966168da6b0c67cda2654072d5
Blastose <blastoisejay@gmail.com>
2023-05-04 14:11:07 -0700

    Use searchParams in user/ routes

--- src/routes/user/[username]/+page.server.ts
+++ src/routes/user/[username]/+page.server.ts
@@ -1,11 +1,16 @@
 import type { PageServerLoad } from './$types';
 import { jsrwrap } from '$lib/server/reddit';
+import type { UserSortOptions, UserTOptions } from 'jsrwrap';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, url }) => {
 	const username = params.username;
 	const jsrwrapUser = jsrwrap.getUser(username);
+	const sort = url.searchParams.get('sort') as UserSortOptions | undefined;
+	const t = url.searchParams.get('t') as UserTOptions | undefined;
 
-	const overview = await jsrwrapUser.getOverview();
+	const options = { sort, t };
+
+	const overview = await jsrwrapUser.getOverview(options);
 
 	return { overview };
 }) satisfies PageServerLoad;
--- src/routes/user/[username]/[userWhere=userWhere]/+page.server.ts
+++ src/routes/user/[username]/[userWhere=userWhere]/+page.server.ts
@@ -1,27 +1,32 @@
 import type { PageServerLoad } from './$types';
 import { jsrwrap } from '$lib/server/reddit';
+import type { UserSortOptions, UserTOptions } from 'jsrwrap';
 import type { SubmissionData, Comment } from 'jsrwrap/types';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, url }) => {
 	const username = params.username;
 	const where = params.userWhere;
 	const jsrwrapUser = jsrwrap.getUser(username);
+	const sort = url.searchParams.get('sort') as UserSortOptions | undefined;
+	const t = url.searchParams.get('t') as UserTOptions | undefined;
+
+	const options = { sort, t };
 
 	let creations;
 	if (where === 'comments') {
-		creations = await jsrwrapUser.getComments();
+		creations = await jsrwrapUser.getComments(options);
 		creations = creations.map((v) => {
 			return { ...v, type: 'comment' };
 		});
 	} else if (where === 'submitted') {
-		creations = await jsrwrapUser.getSubmitted();
+		creations = await jsrwrapUser.getSubmitted(options);
 		creations = creations.map((v) => {
 			return { ...v, type: 'post' };
 		});
 	} else if (where === 'gilded') {
 		creations = await jsrwrapUser.getGilded();
 	} else {
-		creations = await jsrwrapUser.getOverview();
+		creations = await jsrwrapUser.getOverview(options);
 	}
 
 	creations = creations as (
