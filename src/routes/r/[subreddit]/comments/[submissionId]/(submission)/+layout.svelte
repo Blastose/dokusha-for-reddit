3b18b872570bbfc85e14cf51937951fca4ce9787
Blastose <blastoisejay@gmail.com>
2023-05-04 14:47:23 -0700

    Fix skeleton loading showing in SSR'd request

--- src/routes/r/[subreddit]/comments/[submissionId]/(submission)/+layout.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/(submission)/+layout.svelte
@@ -5,11 +5,16 @@
 	import Fly from '$lib/components/layout/Fly.svelte';
 	import { page } from '$app/stores';
 	import SubmissionBodySkeleton from '$lib/components/submission/SubmissionBodySkeleton.svelte';
+	import type { SubmissionData } from 'jsrwrap/types';
 
 	export let data;
 
 	$: key = $page.params.submissionId?.toLowerCase();
 	$: submission = data.streamed.submission;
+
+	// This is only used to prevent TS error below when we know the
+	// streamed data is SSR'd and will not be a promise
+	$: awaitedSubmission = data.streamed.submission as SubmissionData;
 
 	// We can ts-ignore the following lines since we know that when the page is ssr'd
 	// the promise for submission is resolved
@@ -37,7 +42,11 @@
 				<hr />
 				<slot />
 			{:else}
-				<SubmissionBodySkeleton />
+				{#if data.isDataRequestSubmissionId}
+					<SubmissionBodySkeleton />
+				{:else}
+					<SubmissionBody submission={awaitedSubmission} />
+				{/if}
 				<hr />
 				<slot />
 			{/if}
