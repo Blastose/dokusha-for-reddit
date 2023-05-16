b4131f7d06583ee08068787421d41a39919693cb
Blastose <blastoisejay@gmail.com>
2023-05-16 13:53:53 -0700

    Add comment

--- src/routes/r/[subreddit]/comments/[submissionId]/(submission)/[submissionTitle]/+page.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/(submission)/[submissionTitle]/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	// Duplicate of the +page.svelte one level above
+	// This is needed so the [submissionTitle] slug and no slug are the same
+	// While SvelteKit does have [[optionalSlugs]], I get a matching error when there is another level afterwards
 	import CommentContainer from '$lib/components/comment/CommentContainer.svelte';
 
 	export let data;
