4f22bcbfe8581630c790e9e4603a916fef6371c6
Blastose <blastoisejay@gmail.com>
2023-05-28 19:06:00 -0700

    Update submission when refreshing comments

--- src/routes/api/[subreddit]/comments/[submissionId]/+server.ts
+++ src/routes/api/[subreddit]/comments/[submissionId]/+server.ts
@@ -1,17 +1,18 @@
 import { jsrwrap } from '$lib/server/reddit';
 import { json } from '@sveltejs/kit';
 import type { Sort } from 'jsrwrap/types';
+import type { SubmissionReturnType } from '$lib/types/types';
 
 export const GET = async ({ url, params }) => {
 	const submissionId = params.submissionId;
 	const sort = url.searchParams.get('sort') as Sort | undefined;
 
-	let submission;
+	let submission: SubmissionReturnType;
 	if (sort) {
 		submission = await jsrwrap.getSubmission(submissionId).fetch({ sort });
 	} else {
 		submission = await jsrwrap.getSubmission(submissionId).fetch();
 	}
 
-	return json(submission.comments);
+	return json(submission);
 };
