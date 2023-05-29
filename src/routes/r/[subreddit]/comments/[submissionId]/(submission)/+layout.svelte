ab4c57b2a3949fbff4061eb29823202c4f97ad9a
Blastose <blastoisejay@gmail.com>
2023-05-29 10:40:44 -0700

    Add new comments count display

--- src/routes/r/[subreddit]/comments/[submissionId]/(submission)/+layout.svelte
+++ src/routes/r/[subreddit]/comments/[submissionId]/(submission)/+layout.svelte
@@ -6,6 +6,9 @@
 	import { page } from '$app/stores';
 	import SubmissionBodySkeleton from '$lib/components/submission/SubmissionBodySkeleton.svelte';
 	import type { SubmissionData } from 'jsrwrap/types';
+	import { browser } from '$app/environment';
+	import { commentCountStore } from '$lib/stores/commentCountStore.js';
+	import { subredditStore } from '$lib/stores/subredditStore.js';
 
 	export let data;
 
@@ -30,6 +33,27 @@
 			submissionStore.set(await submission);
 		}
 	});
+
+	// We need the inital num comments from the store to display the (`#` new) in the body
+	// since we update it when the submission store changes
+	const initialNumComments = $commentCountStore[$page.params.submissionId] as number | undefined;
+
+	$: if (browser && $submissionStore) {
+		commentCountStore.update((v) => {
+			v[$submissionStore!.id] = $submissionStore!.num_comments;
+			return v;
+		});
+
+		// We want to iterate over the cached subreddit posts and update the number of comments
+		for (const key of Object.keys($subredditStore)) {
+			const foundSubmission = $subredditStore[key]?.find((v) => {
+				return v.id === $page.params.submissionId;
+			});
+			if (foundSubmission) {
+				foundSubmission.num_comments = $submissionStore.num_comments;
+			}
+		}
+	}
 </script>
 
 <svelte:head><title>{title}</title></svelte:head>
@@ -38,14 +62,14 @@
 	<main class="container mx-auto">
 		<section class="flex flex-col gap-4">
 			{#if $submissionStore}
-				<SubmissionBody submission={$submissionStore} />
+				<SubmissionBody submission={$submissionStore} {initialNumComments} />
 				<hr />
 				<slot />
 			{:else}
 				{#if data.isDataRequestSubmissionId}
 					<SubmissionBodySkeleton />
 				{:else}
-					<SubmissionBody submission={awaitedSubmission} />
+					<SubmissionBody submission={awaitedSubmission} {initialNumComments} />
 				{/if}
 				<hr />
 				<slot />
