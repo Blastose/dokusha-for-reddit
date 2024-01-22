b5ea26f48a6e288626dbfbd058f049e3c246867d
Blastose <blastoisejay@gmail.com>
2024-01-21 18:23:00 -0800

    Add cors

--- src/routes/api/[subreddit]/sidebar/+server.ts
+++ src/routes/api/[subreddit]/sidebar/+server.ts
@@ -6,5 +6,13 @@ export const GET = async ({ params }) => {
 
 	const res = await jsrwrap.getSubreddit(subreddit).getSidebar();
 
-	return json(res);
+	return json(res, {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': 'true',
+			'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+			'Access-Control-Allow-Headers':
+				'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+		}
+	});
 };
